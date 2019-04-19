4a50da1d2e3ce182b2c5307d029f86cc0081f73c
A. Mioranza <amioranza@mdcnet.ninja>
2019-04-19 19:58:50 -0300

    Changed Rancher version to eht stable version

--- deploy_rancher.sh
+++ deploy_rancher.sh
@@ -2,4 +2,4 @@
 sudo apt-get update -qq > /dev/null 2>&1 
 sudo apt-get -y install docker.io
 sudo mkdir -p /opt/rancher
-sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 -v /opt/rancher:/var/lib/rancher rancher/rancher:v2.2.0
+sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 -v /opt/rancher:/var/lib/rancher rancher/rancher:stable
